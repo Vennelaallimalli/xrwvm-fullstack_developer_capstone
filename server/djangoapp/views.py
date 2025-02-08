5f1b6832f18a0ae24e14eae70d4cd4155324298b
vennela <vennelaallimalli@gmail.com>
2025-02-08 05:34:34 -0500

    Fixed linting issues

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -118,10 +118,10 @@ def get_dealer_details(request, dealer_id):
 def add_review(request):
     if(request.user.is_anonymous == False):
         data = json.loads(request.body)
-        try:
+        try: 
             response = post_review(data)
             return JsonResponse({"status":200})
         except:
             return JsonResponse({"status":401,"message":"Error in posting review"})
-    else:
+    else: 
         return JsonResponse({"status": 403,"message":"Unauthorized"})
